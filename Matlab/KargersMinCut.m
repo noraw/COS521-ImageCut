0a98ec4bb29697289ade1dd046b84517d0012a03
nicholasturner1 <nturner.stanford@gmail.com>
2015-01-08 21:08:28 -0500

    Modifying Kargers for sparse graphs

--- Matlab/KargersMinCut.m
+++ Matlab/KargersMinCut.m
@@ -19,7 +19,7 @@ function [ids, value] = KargersMinCut(IG, num_iterations, k)
 	cut_edges = sum(sum(final_graphs)) / 2;
 	[value index] = min(cut_edges(:));
 
-	ids = make_ids(memberships{index}, s(1));
+	ids = format_ids(memberships{index}, s(1));
 
 end
 
@@ -34,23 +34,8 @@ function [IG, ids] = KargerIter(IG, k, ids)
 	while s(1) > terminating_number_vertices
 		noSelfLoops = IG - diag(diag(IG));
 
-		total_sum = sum(noSelfLoops(:));
-		cumulative_sum = cumsum(noSelfLoops(:));
 
-		% Selecting random edge
-		threshold_value = rand() * total_sum;
-		i = find(cumulative_sum > threshold_value, 1);
-
-		%Finding coordinates of selected edge
-		r = mod(i,s(1));
-		c = ceil(i / s(1));
-		if(r == 0)
-			r = s(1);
-		end
-
-	%disp(i)
-	%disp(r)
-	%disp(c)
+		r,c = select_random_edge(noSelfLoops);
 
 		[IG, ids] = contractGraph(noSelfLoops, r, c, ids);
 
@@ -63,7 +48,7 @@ function [IG, ids] = KargerIter(IG, k, ids)
 		[IG1 ids1] = KargerIter(IG, k, ids);
 		[IG2 ids2] = KargerIter(IG, k, ids);
 
-		%Determining min cut by remaining edges left
+		%Determining min cut by number of cut edges
 		sum1 = sum(sum(IG1));
 		sum2 = sum(sum(IG2));
 
@@ -78,37 +63,107 @@ function [IG, ids] = KargerIter(IG, k, ids)
 
 end
 
-function [new_graph, ids] = contractGraph(IG, r, c, ids)
+function [i,j] = select_random_edge(graph)
 
-	s = size(IG);
+	[ii,jj,ss] = find(graph);
+
+	total_sum = sum(ss);
+	threshold_value = rand() * total_sum;
+
+	index = 0;
+	while threshold_value > 0
+
+		index = index + 1;
+		threshold_value = threshold_value - ss(index);
+
+	end
+
+	i = ii(index);
+	j = jj(index);
+
+end
+
+function [new_graph, ids] = contractGraph(graph, r, c, ids)
+
+	s = size(graph);
+	[i,j,values] = find(graph);
 	s_new = s-1;
-
-	%disp('coords')
-	%disp(r)
-	%disp(c)
 
 	ind = ones( s(1), 1 );
 	ind = logical(ind);
 	ind(r) = 0;
 	ind(c) = 0;
 
-	%disp('sizes')
-	%disp(s)
-	%disp(size(ind))
-	%disp(size(ids))
-
 	ids = [ {[ids{not(ind)}]} ids{ind} ];
 
-	new_graph = zeros(s_new);
+	% Combining the indices of the selected edge, and sorting them
+	%  in line with the new ids
+	[i,j,values] = combine_indices(r,c, i,j);
 
-	new_graph(1,:) = [0  (IG(r,ind) + IG(c,ind))];
-	new_graph(:,1) = [0; (IG(ind,r) + IG(ind,c))];
+	% Summing duplicate entries
+	[i,j,values] = consolidate_combined_edges(i,j,values);
 
-	new_graph(2:end, 2:end) = IG(ind, ind);
+	% Creating new sparse graph
+	new_graph = sparse(i,j,values, s(1),s(2));
+
+	% Removing self loop
+	new_graph(1,1) = 0;
 
 end
 
-function ids = make_ids(membership_array, num_vertices)
+function [i,j,values] = combine_indices(index1, index2, i,j)
+
+	i(i == index1) = 0;
+	j(j == index1) = 0;
+
+	i(i > index1) = i(i > index1) - 1;
+	j(j > index1) = j(j > index1) - 1;
+
+	if index2 > index1
+		index2 = index2-1;
+	end
+
+	i(i == index2) = 0;
+	j(j == index2) = 0;
+
+	i(i > index2) = i(i > index2) - 1;
+	j(j > index2) = j(j > index2) - 1;
+
+	% r -> 1, c -> 1, everything else shifts up one
+	% in line with the ids
+	i = i + 1;
+	j = j + 1;
+
+end
+
+function [i,j,values] = consolidate_combined_edges(i,j,values)
+
+	paired_indices = [i j];
+
+	s = size(paired_indices);
+
+	[sorted_paired_indices locations] = sortrows(paired_indices);
+	duplicates = [];
+
+	for row = 1:(s(1) - 1)
+
+		if min(sorted_paired_indices(row,:) == sorted_paired_indices(row+1,:))
+			row_locations = [locations(row) locations(row+1)];
+			duplicates = [duplicates; row_locations];
+		end
+
+	end
+
+	% Summing duplicate entry duplicates within first slot
+	values(duplicates(:,1)) = values(duplicates(:,1)) + values(duplicates(:,2));
+	% Removing second duplicate entry from all lists
+	i(duplicates(:,2)) = [];
+	j(duplicates(:,2)) = [];
+	values(duplicates(:,2)) = [];
+
+end
+
+function ids = format_ids(membership_array, num_vertices)
 
 	ids = zeros(1, num_vertices);
 
